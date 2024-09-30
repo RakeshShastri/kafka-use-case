f75753b9e93cb789f2c67a9de1a60a1542b45a56
RakeshShastri <70093416+RakeshShastri@users.noreply.github.com>
2024-09-30 16:51:05 +0530

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -6,8 +6,9 @@ import mysql.connector
 
 
 consumer = def kafka_consumer():
+    consumer = KafkaConsumer('stock_topic', bootstrap_servers=['localhost:9092'], value_deserializer=lambda x: json.loads(x.decode('utf-8')))
     while True:
-        consumer = KafkaConsumer('stock_topic', bootstrap_servers=['localhost:9092'], value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+        
     
         for message in consumer:
             print(message)
