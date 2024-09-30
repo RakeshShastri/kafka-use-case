2f3302dc0e48b39f35386c9a79d3a7d36017686d
RakeshShastri <70093416+RakeshShastri@users.noreply.github.com>
2024-09-30 16:43:26 +0530

    consumer

---
+++ consumer.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from kafka import KafkaProducer, KafkaConsumer
+import json
+from datetime import datetime
+import mysql.connector
+
+
+def kafka_consumer():
+    consumer = KafkaConsumer('stock_topic',
+                             bootstrap_servers=['localhost:9092'],
+                             value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+    
+    for message in consumer:
+        print(message)
+        print(message.value)
