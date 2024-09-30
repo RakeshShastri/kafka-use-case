e58d76ca6ed61ecece76066ee716c9833302402c
RakeshShastri <70093416+RakeshShastri@users.noreply.github.com>
2024-09-30 16:59:04 +0530

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -3,10 +3,9 @@ import json
 
 # Kafka consumer configuration
 consumer = KafkaConsumer(
-    'your_kafka_topic',  # Topic to subscribe to
+    'stock_data',  # Topic to subscribe to
     bootstrap_servers=['localhost:9092'],  # Broker address
     auto_offset_reset='earliest',  # Read from the beginning if no offsets are present
-    group_id='your_consumer_group',  # Consumer group name
     value_deserializer=lambda x: json.loads(x.decode('utf-8'))  # Deserialize JSON
 )
 
