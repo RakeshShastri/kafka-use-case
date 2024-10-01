cc01f033e7a7fe9398b984756c1149ab4f1f2666
RakeshShastri <70093416+RakeshShastri@users.noreply.github.com>
2024-10-01 11:22:32 +0530

    Update producer.py

--- producer.py
+++ producer.py
@@ -5,31 +5,31 @@ from kafka import KafkaProducer
 
 # Kafka producer configuration
 producer = KafkaProducer(
-    bootstrap_servers='localhost:9092',  # Replace with your Kafka broker address
-    value_serializer=lambda v: json.dumps(v).encode('utf-8')  # Serialize as JSON
+    bootstrap_servers='localhost:9092',  
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')  
 )
 
-# CSV file path
+
 csv_file = './stock_data_new.csv'
 
-# Kafka topic name
+
 topic = 'stock_data'
 
-# Read the CSV file using pandas
+
 df = pd.read_csv(csv_file)
 
-# Iterate over the rows of the DataFrame
+
 for _, row in df.iterrows():
-    # Convert the row to a dictionary
+   
     row_dict = row.to_dict()
     
-    # Send the dictionary as JSON to Kafka
+   
     producer.send(topic, value=row_dict)
-    print(f"Sent: {json.dumps(row_dict)}")  # For debugging purposes
+    print(f"Sent: {json.dumps(row_dict)}")  
     
-    # Wait for 1 second before sending the next message
+   
     time.sleep(2)
 
-# Ensure all messages are sent
+
 producer.flush()
 producer.close()
